b7d82f4230e46810435840b63f8e96d433506e12
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2020-01-18 17:26:36 -0300

    falta implementar deploy con heroku

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
+const cors = require('cors');
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors());
 app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'))
 
